66b42c206196f6500d0c26384106f108ae018008
Chanatda Prasawang <64160045@go.buu.ac.th>
2024-12-02 23:24:45 +0700

    Add project files.

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hour = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sta = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.minute = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 53);
+            this.label3.TabIndex = 3;
+            // 
+            // hour
+            // 
+            this.hour.AutoSize = true;
+            this.hour.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hour.Location = new System.Drawing.Point(320, 9);
+            this.hour.Name = "hour";
+            this.hour.Size = new System.Drawing.Size(40, 53);
+            this.hour.TabIndex = 4;
+            this.hour.Text = "h";
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Location = new System.Drawing.Point(93, 9);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(81, 53);
+            this.date.TabIndex = 5;
+            this.date.Text = "date";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Thistle;
+            this.button1.Font = new System.Drawing.Font("TH Sarabun New", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(408, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sta
+            // 
+            this.sta.AutoSize = true;
+            this.sta.Font = new System.Drawing.Font("TH SarabunPSK", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sta.Location = new System.Drawing.Point(111, 207);
+            this.sta.Name = "sta";
+            this.sta.Size = new System.Drawing.Size(0, 53);
+            this.sta.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // minute
+            // 
+            this.minute.AutoSize = true;
+            this.minute.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minute.Location = new System.Drawing.Point(378, 9);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(48, 53);
+            this.minute.TabIndex = 10;
+            this.minute.Text = "m";
+            // 
+            // second
+            // 
+            this.second.AutoSize = true;
+            this.second.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second.Location = new System.Drawing.Point(438, 9);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(35, 53);
+            this.second.TabIndex = 11;
+            this.second.Text = "s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(357, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 53);
+            this.label4.TabIndex = 12;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("TH Sarabun New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(418, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 53);
+            this.label5.TabIndex = 13;
+            this.label5.Text = ":";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WeatherApp.Properties.Resources.schedule;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(390, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(498, 330);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.minute);
+            this.Controls.Add(this.sta);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.hour);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WeatherApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hour;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label sta;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label minute;
+        private System.Windows.Forms.Label second;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Collections.Specialized.BitVector32;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async Task LoadWeatherData()
+        {
+            string apiUrl = "https://data.tmd.go.th/api/Weather3Hours/V2/?uid=api&ukey=api12345"; // URL API
+            string userToken = "X3XI70WyafTlKuThWG0XQUQdTZ0VBR9E"; // User Token
+            string stationName = textBox1.Text.Trim(); // รับชื่อสถานีจาก TextBox1
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // เพิ่ม Header api-key
+                    client.DefaultRequestHeaders.Add("api-key", userToken);
+
+                    // ดึงข้อมูลจาก API
+                    var response = await client.GetStringAsync(apiUrl);
+                    //XDocument xml = XDocument.Parse(response); // แปลง XML
+
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(response);
+
+                    DataTable dataTable = new DataTable();
+                    dataTable.Columns.Add("Station"); // เพิ่มคอลัมน์ตามโครงสร้าง XML
+                    dataTable.Columns.Add("Temperature");
+                    dataTable.Columns.Add("Humidity");
+
+                    foreach (XmlNode node in xmlDoc.SelectNodes("//Station"))
+                    {
+                        DataRow row = dataTable.NewRow();
+                        string stationNameThai = node.SelectSingleNode("StationNameThai")?.InnerText;
+                        row["Station"] = stationNameThai;
+
+                        if (!string.IsNullOrEmpty(stationName) && !string.Equals(stationNameThai, stationName, StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue; // ข้ามถ้าไม่ตรงกับชื่อสถานีที่ป้อน
+                        }
+
+                        var observationNode = node.SelectSingleNode("Observation");
+                        if (observationNode != null)
+                        {
+                            row["Temperature"] = observationNode.SelectSingleNode("AirTemperature")?.InnerText;
+                            row["Humidity"] = observationNode.SelectSingleNode("RelativeHumidity")?.InnerText;
+                        }
+                        else
+                        {
+                            row["Temperature"] = "N/A";
+                            row["Humidity"] = "N/A";// กรณีไม่มีโหนด Observation
+                        }
+
+                        dataTable.Rows.Add(row);
+                    }
+
+
+
+                    if (dataTable != null)
+                    {
+                        dataGridView1.DataSource = dataTable; // แสดงเฉพาะคอลัมน์ที่เลือกใน DataGridView
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"เกิดข้อผิดพลาด: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await LoadWeatherData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            date.Text = now.ToString("dd/MM/yyyy");
+            hour.Text = now.Hour.ToString("00"); // แสดงชั่วโมง
+            minute.Text = now.Minute.ToString("00"); // แสดงนาที
+            second.Text = now.Second.ToString("00"); // แสดงวินาที
+
+
+            if (now.Second <= 30)
+            {
+                second.ForeColor = Color.Green; // วินาที <= 30 สีเขียว
+            }
+            else
+            {
+                second.ForeColor = Color.Red; // วินาที > 30 สีแดง
+            }
+
+            if (now.Second == 30 || now.Second == 0)
+            {
+                 LoadWeatherData();
+            }
+        }
+    }
+}
